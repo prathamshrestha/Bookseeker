80b37ae4357458babdff53eae191a6c804f15ad6
Pratham <prathamshrestha358@gmail.com>
2020-10-09 21:45:15 +0545

    frontend fixing issues

--- bookseeker_be/sell_books/models.py
+++ bookseeker_be/sell_books/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+def upload_path(instance, filename):
+    return '/'.join(['bookpic',str(instance.name), filename])
+
 class booksell_model(models.Model):
     name=models.CharField(max_length=50)
-    bookpicture=models.ImageField()
+    bookpicture=models.ImageField(blank=True, upload_to=upload_path)
     age = models.IntegerField(blank=True)
     description=models.CharField(max_length=500)
     status=models.DateField(blank=True)
